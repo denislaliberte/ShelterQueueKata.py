eeb8678bec5654e1001905cd62724976c23b68a3
Denis Laliberté <denis@tp1.ca>
2015-01-25 23:54:44 -0500

    dequeue one item

--- ShelterQueue.py
+++ ShelterQueue.py
@@ -4,7 +4,9 @@ class ShelterQueue:
     return self.count == 0
 
   def enqueue(self,item):
+    self.item = item
     self.count += 1
 
   def dequeueAny(self):
     self.count -= 1
+    return self.item
--- ShelterQueue_test.py
+++ ShelterQueue_test.py
@@ -10,7 +10,7 @@ class ShelterQueue_test(unittest.TestCase):
     queue = ShelterQueue()
     queue.enqueue(1)
     self.assertEqual(queue.empty(),False)
-    queue.dequeueAny()
+    self.assertEqual(queue.dequeueAny(),1)
     self.assertEqual(queue.empty(),True)
 
 
