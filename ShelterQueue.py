6e8c438be79a112891ad3837a543e89030b8e5d8
Denis Laliberté <denis@tp1.ca>
2015-01-25 23:50:02 -0500

    count one item

--- ShelterQueue.py
+++ ShelterQueue.py
@@ -1,3 +1,7 @@
 class ShelterQueue:
+  count = 0
   def empty(self):
-    return True
+    return self.count == 0
+
+  def enqueue(self,item):
+    self.count += 1
--- ShelterQueue_test.py
+++ ShelterQueue_test.py
@@ -5,3 +5,9 @@ class ShelterQueue_test(unittest.TestCase):
   def test_empty_queue(self):
     queue = ShelterQueue()
     self.assertEqual(queue.empty(),True)
+
+  def test_one_item(self):
+    queue = ShelterQueue()
+    queue.enqueue(1)
+    self.assertEqual(queue.empty(),False)
+
