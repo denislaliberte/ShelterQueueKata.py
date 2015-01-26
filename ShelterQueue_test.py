48e936fcd4a4fa2f120da996e76285a328805e03
Denis Laliberté <denis@tp1.ca>
2015-01-26 12:35:47 -0500

    initialise object in before hook

--- ShelterQueue_test.py
+++ ShelterQueue_test.py
@@ -2,31 +2,29 @@ import unittest
 from ShelterQueue import ShelterQueue
 
 class ShelterQueue_test(unittest.TestCase):
+  def setup_method(self, method):
+    self.queue = ShelterQueue()
   def test_empty_queue(self):
-    queue = ShelterQueue()
-    self.assertEqual(queue.empty(),True)
+    self.assertEqual(self.queue.empty(),True)
 
   def test_one_item(self):
-    queue = ShelterQueue()
-    queue.enqueue(1)
-    self.assertEqual(queue.empty(),False)
-    self.assertEqual(queue.dequeueAny(),1)
-    self.assertEqual(queue.empty(),True)
+    self.queue.enqueue(1)
+    self.assertEqual(self.queue.empty(),False)
+    self.assertEqual(self.queue.dequeueAny(),1)
+    self.assertEqual(self.queue.empty(),True)
 
   def test_two_item(self):
-    queue = ShelterQueue()
-    queue.enqueue(1)
-    queue.enqueue(2)
-    self.assertEqual(queue.dequeueAny(),1)
-    self.assertEqual(queue.empty(),False)
-    self.assertEqual(queue.dequeueAny(),2)
-    self.assertEqual(queue.empty(),True)
+    self.queue.enqueue(1)
+    self.queue.enqueue(2)
+    self.assertEqual(self.queue.dequeueAny(),1)
+    self.assertEqual(self.queue.empty(),False)
+    self.assertEqual(self.queue.dequeueAny(),2)
+    self.assertEqual(self.queue.empty(),True)
 
   def test_dequeue_dog(self):
-    queue = ShelterQueue()
-    queue.enqueue({"type":"cat","name":"larry"})
-    queue.enqueue({"type":"dog","name":"fido"})
-    self.assertEqual(queue.dequeueDog(),{"type":"dog","name":"fido"})
-    self.assertEqual(queue.dequeueAny(),{"type":"cat","name":"larry"})
+    self.queue.enqueue({"type":"cat","name":"larry"})
+    self.queue.enqueue({"type":"dog","name":"fido"})
+    self.assertEqual(self.queue.dequeueDog(),{"type":"dog","name":"fido"})
+    self.assertEqual(self.queue.dequeueAny(),{"type":"cat","name":"larry"})
 
 
